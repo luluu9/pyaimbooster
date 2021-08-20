bdedef78d44d23a2c9c1e4ef74b5a7d5948bb42b
Mateusz Naklicki <mateusz.naklicki06@gmail.com>
2021-08-21 01:54:28 +0200

    Code refactoring

--- src/aimbooster.py
+++ src/aimbooster.py
@@ -4,33 +4,10 @@ import random
 import time
 from pygame.constants import USEREVENT
 
-
 from pygame.locals import (
     QUIT,
 )
 
-pygame.init()
-pygame.display.set_caption('aimbooster v.0.0.1')
-
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-FPS = 60
-
-TARGET_SPAWNRATE = 3 # targets per second 
-
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-clock = pygame.time.Clock()
-
-ADD_TARGET = USEREVENT + 1
-pygame.time.set_timer(ADD_TARGET, int(1000/TARGET_SPAWNRATE))
-
-scoreCounter = None
-
-# APPEARANCE 
-background_color = (222, 222, 222)
-outline_color = (0, 0, 0) 
-filling_color = (255, 255, 255)
-score_color = (74, 74, 74)
 
 class ScoreCounter():
     def __init__(self):
@@ -65,7 +42,7 @@ class Target():
     def __init__(self):
         self.max_radius = 50
         self.reached_max = False
-        self.pos = get_random_pos(self.max_radius)
+        self.pos = self.get_random_pos(self.max_radius)
         self.radius = 0
         self.outline_margin = 4
 
@@ -91,54 +68,109 @@ class Target():
     def destroy(self):
         targets_to_delete.append(self)
         scoreCounter.add_target()
+    
+    def get_random_pos(self, margin=0):
+        x = random.randint(margin, SCREEN_WIDTH-margin)
+        y = random.randint(margin, SCREEN_HEIGHT-margin)
+        return (x, y)
 
 
-def get_random_pos(margin=0):
-    x = random.randint(margin, SCREEN_WIDTH-margin)
-    y = random.randint(margin, SCREEN_HEIGHT-margin)
-    return (x, y)
+class Game():
+    def __init__(self):
+        self.change_game_mode("lobby")
+
+    def change_game_mode(self, game_mode):
+        if game_mode == "lobby":
+            self.load_lobby()
+        elif game_mode == "arcade":
+            self.load_arcade()
+        else:
+            raise Exception("There is no provided game mode: " + str(game_mode))
+        self.game_mode = game_mode
+    
+    def load_lobby(self):
+        pass
+
+    def load_arcade(self):
+        global scoreCounter, targets, targets_to_delete
+        scoreCounter = ScoreCounter()
+        targets = [Target()]
+        targets_to_delete = []
+        pygame.time.set_timer(ADD_TARGET, int(1000/TARGET_SPAWNRATE))
+    
+    def frame(self):
+        if self.game_mode == "lobby":
+            self.lobby_frame()
+        elif self.game_mode == "arcade":
+            self.arcade_frame()
+    
+    def lobby_frame(self):
+        pass
+
+    def arcade_frame(self):
+        screen.fill(background_color)
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                for target in targets:
+                    target.check_collision(pygame.mouse.get_pos())
+                scoreCounter.add_shoot()
+            elif event.type == ADD_TARGET:
+                targets.append(Target())
+                
+        # update targets size
+        for target in targets:
+            target.update()
+        
+        # delete unused targets
+        while len(targets_to_delete) > 0:
+            try:
+                targets.remove(targets_to_delete.pop())
+            except ValueError: # probably double clicked faster than delta time
+                pass 
+        # update counter
+        scoreCounter.update()
 
 
-def get_random_color():
-    r = random.randint(0, 255)
-    g = random.randint(0, 255)
-    b = random.randint(0, 255)
-    return (r, g, b)
+# BASE SETTINGS
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+FPS = 60
 
+# PYGAME INIT
+pygame.init()
+pygame.display.set_caption('aimbooster v.0.0.1')
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+clock = pygame.time.Clock()
 
-scoreCounter = ScoreCounter()
+# APPEARANCE 
+background_color = (222, 222, 222)
+outline_color = (0, 0, 0) 
+filling_color = (255, 255, 255)
+score_color = (74, 74, 74)
 
-targets = [Target()]
+# GAME MECHANICS
+scoreCounter = None
+targets = []
 targets_to_delete = []
+TARGET_SPAWNRATE = 3 # targets per second 
 
+# GAME EVENTS
+ADD_TARGET = USEREVENT + 1
+
+# LOAD GAME
+game = Game()
+game.change_game_mode("arcade")
+
+# MAINLOOP
 running = True
 while running:
-    screen.fill(background_color)
-    for event in pygame.event.get():
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            for target in targets:
-                target.check_collision(pygame.mouse.get_pos())
-            scoreCounter.add_shoot()
-        elif event.type == QUIT:
+    for event in pygame.event.get(QUIT):
+        if event.type == QUIT:
             running = False
-        elif event.type == ADD_TARGET:
-            targets.append(Target())
-            
-    # update targets size
-    for target in targets:
-        target.update()
-    
-    # delete unused targets
-    while len(targets_to_delete) > 0:
-        targets.remove(targets_to_delete.pop())
-    
-    # update counter
-    scoreCounter.update()
-
+    game.frame()
     # refresh display
     pygame.display.update()
     clock.tick(FPS)
-
 pygame.quit()
 
 
