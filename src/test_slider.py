adf945451b70410c47eb85ed200171d9e1b0a8f3
Mateusz Naklicki <mateusz.naklicki06@gmail.com>
2021-09-10 01:44:54 +0200

    Refresh screen each frame

--- src/test_slider.py
+++ src/test_slider.py
@@ -15,13 +15,15 @@ screen.fill(SETTINGS.Appearance.summary_bg_color, pygame.Rect(0, 0, 800, 600))
 font = pygame.freetype.Font(SETTINGS.Appearance.default_font, 20)
 slider = components.Slider(screen, font, (0, 0, 0), 15, (0, 0, 0), 0, 100, 50, 5, 5, SETTINGS.Appearance.background_color, screen.get_rect().center, (150, 20))
 slider.center = screen.get_rect().center
-slider.draw()
+
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             break
         elif event.type == pygame.MOUSEBUTTONDOWN:
             slider.is_clicked(pygame.mouse.get_pos())
+    screen.fill(SETTINGS.Appearance.background_color)
+    slider.draw()
     pygame.display.update()
     clock.tick(SETTINGS.FPS)
 pygame.quit()
